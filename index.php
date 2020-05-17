0985a3f652a2aeb614970eff6a4dd08b69cdba94
juliaokataleko <juliofeli78@gmail.com>
2020-05-17 12:19:46 +0000

    Updating index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 
-$directory = './';
-
-$file = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
-
-while($file->valid()) {
-
-    if (!$file->isDot()) {
-        if(substr($file->getSubPathName(), 0, 4) != '.git')
-            echo "<a href='" . $file->getSubPathName() . "'>" . $file->getSubPathName() . "</a><br>";
-    }
-
-    $file->next();
-}
+echo "<h1>Wellcome to PHP Lessons</h1>";
